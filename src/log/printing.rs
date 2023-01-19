3b2c2f78d66b3dc657ff3f38107a3449896dcb84
Grzegorz Bujak <grzegorzbujak10@gmail.com>
2023-01-19 19:00:25 +0100

    Simplify printing

--- src/log/printing.rs
+++ src/log/printing.rs
@@ -1,9 +1,9 @@
-use super::{weekday_strs, LogElement};
+use super::{weekday_strs, LogElement, WEEKDAYS};
 use chrono::Datelike;
 use itertools::Itertools;
-use prettytable::{row, Cell, Table};
+use prettytable::{row, Cell, Row, Table};
 
-pub fn print_logs_table(mut logs: Vec<LogElement>) {
+pub fn print_logs_table(logs: &Vec<LogElement>) {
     let mut table = Table::new();
     let mut header = row!["Ticket"];
     for weekday_str in &weekday_strs() {
@@ -11,22 +11,26 @@ pub fn print_logs_table(mut logs: Vec<LogElement>) {
     }
     table.add_row(header);
 
-    logs.sort_by_key(|it| (it.ticket.clone(), it.date.weekday().number_from_monday()));
+    let unique_tickets = logs.iter().map(|it| &it.ticket).unique();
+    let tickets_days = logs
+        .iter()
+        .into_group_map_by(|it| (it.date.weekday().num_days_from_monday(), it.ticket.as_str()));
 
-    for (ticket, logs) in logs.iter().group_by(|it| &it.ticket).into_iter() {
-        let mut row = row![ticket];
-        for (_weekday, logs_for_weekday) in logs
-            .into_iter()
-            .group_by(|it| it.date.weekday())
-            .into_iter()
-        {
-            row.add_cell(Cell::new(
-                &logs_for_weekday
-                    .into_iter()
-                    .map(|it| format!("{}h {}", it.hours, it.tag.clone().unwrap_or("".into())))
-                    .join("\n"),
-            ))
+    for ticket in unique_tickets {
+        let mut row = Row::empty();
+        row.add_cell(Cell::new(ticket));
+
+        for weekday in WEEKDAYS {
+            let cell_text = tickets_days
+                .get(&(weekday.num_days_from_monday(), ticket))
+                .unwrap_or(&vec![])
+                .iter()
+                .map(|it| format!("{}h {}", it.hours, it.tag.as_deref().unwrap_or("")))
+                .join("\n");
+
+            row.add_cell(Cell::new(&cell_text));
         }
+
         table.add_row(row);
     }
 
